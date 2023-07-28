02facea383c7d7c213d18c98784ec2e02cbb7c8a
Victor <linkgamesplays@gmail.com>
2023-07-28 10:32:24 -0300

    chore: create logic for store product in database

--- backend/src/controllers/product-controller.ts
+++ backend/src/controllers/product-controller.ts
@@ -1,30 +1,47 @@
-import { v4 as uuidv4 } from 'uuid';
-import { IProduct } from '../@types/IProduct'
-import { Product } from '../models/Product'
+import { IProduct } from '../@types/IProduct';
+import { Product } from '../models/Product';
 import { Request, Response } from 'express';
+import { generateId } from '../utils/random-bytes';
 
-const randomId: string[] = uuidv4().split('-');
+class ProductController {
+	async store(req: Request, res: Response): Promise<void | Response> {
+		const id = generateId();
+		const { name, price, product_type, sector, quantity } = req.body;
 
-class ProductController  {
-  async store(req: Request, res: Response): Promise<void | IProduct>{
-    const id = randomId;
-    const { name, price, product_type, sector } = req.body;
-    
-    const productData: IProduct = {
-      id: id,
-      name: name,
-      price: price,
-      product_type: product_type,
-      sector: sector,
-    }
+		const productData: IProduct = {
+			id: id,
+			name: name,
+			price: price,
+			product_type: product_type,
+			sector: sector,
+			quantity: quantity,
+			total: Number(quantity) * Number(price),
+		};
 
-    try {
-      const product = await Product.create(productData)
-      res.status(200).json({ product_id: id, name: name, price: price, product_type: product_type, sector: sector});
-      return product
-    } catch (err: any) {
-      console.log(err.message)
-    }
-    
-  }
+		try {
+			const productCheck = await Product.findAll({ where: { name: name } });
+			if (productCheck.length > 0) {
+				return res
+					.status(400)
+					.json({ msg: [`${name} has already been registered`] });
+			}
+
+			const product = await Product.create(productData);
+
+			return res.status(201).json({
+				product_id: id,
+				name: name,
+				price: `${price}R$`,
+				product_type: product_type,
+				sector: sector,
+				quantity: quantity,
+				total: `${productData.total}R$`,
+			});
+		} catch (err: any) {
+			console.log(err.message);
+		}
+	}
+
 }
+
+export default new ProductController();
