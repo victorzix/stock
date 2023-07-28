e06b30c0df766ec5804f40bf4888a3c5c8add3f9
victorzix <linkgamesplays@gmail.com>
2023-07-28 16:13:18 -0300

    feat: create index method to list all products

--- backend/src/controllers/product-controller.ts
+++ backend/src/controllers/product-controller.ts
@@ -1,88 +1,109 @@
-import { IProduct } from '../@types/IProduct';
-import { Product } from '../models/Product';
-import { Request, Response } from 'express';
-import { generateId } from '../utils/random-bytes';
+import { IProduct } from "../@types/IProduct";
+import { Product } from "../models/Product";
+import { Request, Response } from "express";
+import { generateId } from "../utils/random-bytes";
 
 class ProductController {
-	async store(req: Request, res: Response): Promise<void | Response> {
-		const id = generateId();
-		const { name, price, product_type, sector, quantity } = req.body;
+  async store(req: Request, res: Response): Promise<void | Response> {
+    const id = generateId();
+    const { name, price, product_type, sector, quantity } = req.body;
 
-		const productData: IProduct = {
-			id: id,
-			name: name,
-			price: price,
-			product_type: product_type,
-			sector: sector,
-			quantity: quantity,
-			total_income: Number(quantity) * Number(price),
-		};
+    const productData: IProduct = {
+      id: id,
+      name: name,
+      price: price,
+      product_type: product_type,
+      sector: sector,
+      quantity: quantity,
+      total_income: Number(quantity) * Number(price),
+    };
 
-		try {
-			const productCheck = await Product.findAll({ where: { name: name } });
-			if (productCheck.length > 0) {
-				return res
-					.status(400)
-					.json({ msg: [`${name} has already been registered`] });
-			}
+    try {
+      const productCheck = await Product.findAll({ where: { name: name } });
+      if (productCheck.length > 0) {
+        return res
+          .status(400)
+          .json({ msg: [`${name} has already been registered`] });
+      }
 
-			const product = await Product.create(productData);
+      const product = await Product.create(productData);
 
-			return res.status(201).json({
-				msg: ['Successfuly added']
-				,
-				product: {
-				product_id: id,
-				name: name,
-				price: price,
-				product_type: product_type,
-				sector: sector,
-				quantity: quantity,
-				total_income: productData.total_income,
-				}
-			});
-		} catch (err: any) {
-			console.log(err.message);
-		}
-	}
+      return res.status(201).json({
+        msg: ["Successfuly added"],
+        product: {
+          product_id: id,
+          name: name,
+          price: price,
+          product_type: product_type,
+          sector: sector,
+          quantity: quantity,
+          total_income: productData.total_income,
+        },
+      });
+    } catch (err: any) {
+      console.log(err.message);
+    }
+  }
 
-	async show(req: Request, res: Response): Promise<void | Response> {
-		const product = await Product.findByPk(req.params.id);
+  async show(req: Request, res: Response): Promise<void | Response> {
+    const product = await Product.findByPk(req.params.id);
 
-		if (!product) {
-			return res.status(400).json({ msg: ['Product not found'] });
-		}
+    if (!product) {
+      return res.status(400).json({ msg: ["Product not found"] });
+    }
 
-		return res.status(200).json({ product: [product] });
-	}
+    return res.status(200).json({ product: [product] });
+  }
 
-	async update(req: Request, res: Response): Promise<void | Response> {
-		const product = await Product.findByPk(req.params.id);
+  async update(req: Request, res: Response): Promise<void | Response> {
+    const product = await Product.findByPk(req.params.id);
 
-		if (!product) {
-			return res.status(400).json({ msg: ['Product not found'] });
-		}
+    if (!product) {
+      return res.status(400).json({ msg: ["Product not found"] });
+    }
 
-		const priceCalc: number = req.body.price || product.price;
-		const quantityCalc: number = req.body.quantity || product.quantity
+    const priceCalc: number = req.body.price || product.price;
+    const quantityCalc: number = req.body.quantity || product.quantity;
 
-		const total_income: number = priceCalc * quantityCalc
-		
-		const productEdited = await product.update({...req.body, total_income});
-		const { name, price, product_type, sector, quantity } = productEdited;
+    const total_income: number = priceCalc * quantityCalc;
 
-		return res.status(200).json({
-			msg: ['Updated successfully'],
-			product: {
-				name: name,
-				price: price,
-				product_type: product_type,
-				sector: sector,
+    const productEdited = await product.update({ ...req.body, total_income });
+    const { name, price, product_type, sector, quantity } = productEdited;
+
+    return res.status(200).json({
+      msg: ["Updated successfully"],
+      product: {
+        name: name,
+        price: price,
+        product_type: product_type,
+        sector: sector,
         quantity: quantity,
-        total_income: total_income
-			},
-		});
-	}
+        total_income: total_income,
+      },
+    });
+  }
+
+  async index(req: Request, res: Response): Promise<void | Response> {
+    const products = await Product.findAll();
+    const listOfProducts = products.map((product: IProduct) => {
+      const { id, name, total_income, quantity, price, sector } = product;
+      return {
+        product: {
+          name: name,
+          id: id,
+          total_income: total_income,
+          quantity: quantity,
+          price: price,
+          sector: sector,
+        },
+      };
+    });
+    return res.status(200).json({
+      products: {
+        listOfProducts: listOfProducts,
+      },
+    });
+  }
 }
 
 export default new ProductController();
