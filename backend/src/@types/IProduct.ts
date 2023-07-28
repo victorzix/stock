902d9937469081efefb331c4d1dce074f03be551
victorzix <linkgamesplays@gmail.com>
2023-07-28 20:36:51 -0300

    feat: create product query interface

--- backend/src/@types/IProduct.ts
+++ backend/src/@types/IProduct.ts
@@ -13,6 +13,12 @@ export interface IProduct {
   total_income?: number;
 };
 
+export interface IProductQuery {
+  name?: string;
+  price?: number;
+  sector?: string;
+}
+
 interface IProductCreation extends Optional<IProduct, 'quantity'> {}
 
 export class ProductInstance extends Model<IProduct, IProductCreation>{
