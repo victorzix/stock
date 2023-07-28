ec0e173ed8195dda8ed0ea699b84e07981bda560
Victor <linkgamesplays@gmail.com>
2023-07-28 10:33:57 -0300

    chore: unninstall uuid package and call function app.use on project routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,17 +1,19 @@
 // Imports
 import express, { Express, Request, Response } from 'express';
+import router from './routes/product-routes';
 
 // Variables declarations
 const app = express();
 
+//app uses
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // app Routes
-
+app.use(router)
 // app Configurations
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World');
+	res.send('Hello World');
 });
-
-
 
 export default app;
