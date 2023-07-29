b475ee09b2ba3f9f600eb266aa9d182e31b2bf10
victorzix <linkgamesplays@gmail.com>
2023-07-28 22:48:32 -0300

    chore: create interfaces for sector model

--- backend/src/@types/ISector.ts
+++ backend/src/@types/ISector.ts
@@ -1,6 +1,26 @@
+import {
+  Model,
+  Optional
+} from "sequelize";
+
 export interface ISector {
-  id: number;
+  id: string;
   name: string;
-  income: number;
-  type: string | null;
+  type: string
+  income?: number;
 };
+
+export interface ISectorQuery {
+  id?: number;
+  name?: string;
+  type?: string;
+}
+
+interface ISectorCreation extends Optional<ISector, 'income'> {}
+
+export class SectorInstance extends Model<ISector, ISectorCreation>{
+  declare id: string;
+  declare name: string;
+  declare type: string | undefined;
+  declare income?: number | undefined;
+}
