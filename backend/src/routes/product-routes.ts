3405714e4f97d4c685aa15ceb3f8a02cb70de707
victorzix <linkgamesplays@gmail.com>
2023-07-28 22:16:47 -0300

    feat: create route for delete

--- backend/src/routes/product-routes.ts
+++ backend/src/routes/product-routes.ts
@@ -8,6 +8,7 @@ router.post('/store', ProductController.store)
 router.get('/show/:id', ProductController.show)
 router.get('/index', ProductController.index)
 router.put('/update/:id', ProductController.update)
+router.delete('/delete/:id', ProductController.delete)
 
 
 export default router;
