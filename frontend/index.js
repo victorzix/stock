4e492ad8ecb70a34a504bffe0107f003ddc39f22
victorzix <linkgamesplays@gmail.com>
2023-08-02 15:40:59 -0300

    feat: add docker-compose archive and updated .gitignore

--- frontend/index.js
+++ frontend/index.js
@@ -1,15 +1,15 @@
-const productsList = document.querySelector('#table_body');
-const registerForm = document.getElementById('product_register');
+const productsList = document.querySelector("#table_body");
+const registerForm = document.getElementById("product_register");
 
-const url = 'http://localhost:3000/products';
+const url = "http://localhost:3000/products";
 
 async function getResponse() {
   const res = await fetch(url);
-	const response = await res.json();
-  
-  productsList.innerHTML = ''
-  
-	response.map((product) => {
+  const response = await res.json();
+
+  productsList.innerHTML = "";
+
+  response.map((product) => {
     productsList.innerHTML += `
     <th scope="row" class="p-3" >${product.id}</th>
     <td class="p-3">${product.sector}</td>
@@ -19,46 +19,58 @@ async function getResponse() {
     <td class="p-3">${product.total_income}</td>
     <td><button class="btn btn-dark btn-sm m-1" id="removeBtn" onclick="deleteProduct(this)" data-id="${product.id}">Remover</button></td>
     `;
-	});
+  });
 }
 
-document.getElementById('addProd').addEventListener('click', () => {
-  registerForm.style.display = 'block';
+document.getElementById("addProd").addEventListener("click", () => {
+  registerForm.style.display = "block";
 });
 
 async function postData(data) {
   const res = await fetch(url, {
-    method: 'POST',
+    method: "POST",
     body: JSON.stringify(data),
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
   });
   return res.json();
 }
 
-registerForm.addEventListener('submit', async (e) => {
+registerForm.addEventListener("submit", async (e) => {
   e.preventDefault();
-  
-	const name = document.querySelector('[name="name"]').value;
-	const quantity = document.querySelector('[name="quantity"]').value;
-	const price = document.querySelector('[name="price"]').value;
-	const sector = document.querySelector('[name="sector"]').value;
-  
+  const inputs = document.querySelectorAll('#inputs')
+  const name = document.querySelector('[name="name"]').value;
+  const quantity = document.querySelector('[name="quantity"]').value;
+  const price = document.querySelector('[name="price"]').value;
+  const sector = document.querySelector('[name="sector"]').value;
+
   const data = {
     name,
     quantity,
     price,
-    sector
-  }
+    sector,
+  };
+
+  
   await postData(data);
-  getResponse()
+  inputs.forEach(element => {
+    element.value = ''
+  });
+  getResponse();
 });
 
-async function deleteProduct(e){
-  const id = e.getAttribute('data-id');
-  const res = await 
+async function deleteProduct(e) {
+  const id = e.getAttribute("data-id");
+  const res = await fetch(`${url}/${id}`, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  });
+  getResponse();
 }
 
-window.addEventListener('load', getResponse());
+window.addEventListener("load", getResponse());
