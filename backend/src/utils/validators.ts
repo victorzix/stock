ed2383803b085af5ad22580366c15923cbef677c
Victor <linkgamesplays@gmail.com>
2023-08-03 09:38:54 -0300

    feat: create data validation with yup package

--- backend/src/utils/validators.ts
+++ backend/src/utils/validators.ts
@@ -1,13 +1,32 @@
-import Joi from "joi";
-import { IProduct } from "../@types/IProduct";
+import { object, string, number, ObjectSchema, setLocale } from 'yup'
 
-export const validateProducts = (product: IProduct) => {
-  const productSchema = Joi.object({
-    name: Joi.string().max(70).required(),
-    quantity: Joi.number().min(1).max(1000).required(),
-    price: Joi.number().min(1).max(5000).required(),
-    sector: Joi.number().min(1).max(120).required()
-  })
-
-  return productSchema.validate(product)
+export interface IValidProduct {
+  id: string;
+  name: string;
+  quantity: number;
+  price: number;
+  sector: string;
 }
+
+setLocale({
+  mixed: {
+    default: 'Product is invalid',
+  },
+  number: {
+    min: ({path}) => `${path} Must have more than 1`
+  },
+  string: {
+     min: "Please insert at least one character",
+     max: ({path, max}) => `${path} Cannot be more than ${max}`
+  }
+})
+
+export const productSchema: ObjectSchema<IValidProduct> = object({
+  id: string().required(),
+  name: string().min(1).max(60).required(),
+  quantity: number().min(1).max(5000).required(),
+  price: number().min(1).max(10000).required(),
+  sector: string().min(0).max(3).required(),
+})
+
+
