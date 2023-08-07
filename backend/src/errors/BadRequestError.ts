35cedd2c36e277139ca91413a93a6bcb7cf3ca17
victorzix <linkgamesplays@gmail.com>
2023-08-07 15:52:56 -0300

    feat: import enum EStatusCode for use

--- backend/src/errors/BadRequestError.ts
+++ backend/src/errors/BadRequestError.ts
@@ -1,7 +1,8 @@
 import { AppError } from "./AppError";
+import EStatusCode from '../@types/EStatusCode'
 
 export class BadRequestError extends AppError {
   constructor(message: string) {
-    super(400, message)
+    super(EStatusCode.BAD_REQUEST, message)
   }
 }
--- backend/src/errors/NotFoundError.ts
+++ backend/src/errors/NotFoundError.ts
@@ -1,7 +1,9 @@
 import { AppError } from "./AppError";
+import EStatusCode from '../@types/EStatusCode'
+
 
 export class NotFoundError extends AppError {
   constructor(message: string) {
-    super(404, message)
+    super(EStatusCode.NOT_FOUND, message)
   }
 }
