c3b117fc4ff734e8597bbf20446714b2bf8babf8
Victor <linkgamesplays@gmail.com>
2023-08-09 11:47:25 -0300

    test: improve validator (not completed)

--- backend/src/controllers/product-controller.ts
+++ backend/src/controllers/product-controller.ts
@@ -1,208 +1,216 @@
-import { IProduct, IProductQuery, ProductInstance } from "../@types/IProduct";
-import { Product } from "../models/Product";
-import { NextFunction, query, Request, Response } from "express";
-import { generateId } from "../utils/random-bytes";
-import { Op, FindOptions } from "sequelize";
+import { IProduct, IProductQuery, ProductInstance } from '../@types/IProduct';
+import { Product } from '../models/Product';
+import { NextFunction, query, Request, Response } from 'express';
+import { generateId } from '../utils/random-bytes';
+import { Op, FindOptions } from 'sequelize';
 import {
-  createProductSchema,
-  isValid,
-  IValidProduct,
-  IValidUpdate,
-  updateProductSchema,
-} from "../utils/validators";
-import { NotFoundError } from "../errors/NotFoundError";
-import { BadRequestError } from "../errors/BadRequestError";
+	createProductSchema,
+	IValidProduct,
+	IValidUpdate,
+	updateProductSchema,
+	validateData,
+} from '../utils/validators';
+import { NotFoundError } from '../errors/NotFoundError';
+import { BadRequestError } from '../errors/BadRequestError';
 
 class ProductController {
-  async store(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ): Promise<Response | void> {
-    const id = generateId();
-    const { name, price, sector, quantity } = req.body;
-    const productData: IProduct = {
-      id,
-      name,
-      price,
-      sector,
-      quantity,
-      total_income: Number(quantity) * Number(price),
-    };
+	async store(
+		req: Request,
+		res: Response,
+		next: NextFunction
+	): Promise<Response | void> {
+		const id = generateId();
+		const { name, price, sector, quantity } = req.body;
+		const productData: IProduct = {
+			id,
+			name,
+			price,
+			sector,
+			quantity,
+			total_income: Number(quantity) * Number(price),
+		};
 
-    const productCheck = await Product.findOne({ where: { name: name } });
-    if (productCheck) {
-      next(new BadRequestError("Product already exists"));
-      return;
-    }
+		const productCheck = await Product.findOne({ where: { name: name } });
+		if (productCheck) {
+			next(new BadRequestError('Product already exists'));
+			return;
+		}
 
-    const validProduct: IProduct = await isValid(createProductSchema, productData)
-    
-    const product = await Product.create(validProduct);
+		const validProduct: IValidProduct | string = await validateData(
+			createProductSchema,
+			productData,
+		);
 
-    return res.status(201).json({
-      message: "Successfully created product",
-      product,
-    });
-  }
+		if (typeof validProduct === 'string') {
+			next(new BadRequestError(validProduct));
+      return
+		}
 
-  async show(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ): Promise<Response | void> {
-    const product = await Product.findByPk(req.params.id);
+		const product = await Product.create(validProduct);
 
-    if (!product) {
-      next(new NotFoundError("Product not found"));
-      return;
-    }
-    return res.status(200).json(product);
-  }
+		return res.status(201).json({
+			message: 'Successfully created product',
+			product,
+		});
+	}
 
-  async update(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ): Promise<Response | void> {
-    const product = await Product.findByPk(req.params.id);
-    if (!product) {
-      next(new NotFoundError("Product not found"));
-      return;
-    }
+	async show(
+		req: Request,
+		res: Response,
+		next: NextFunction
+	): Promise<Response | void> {
+		const product = await Product.findByPk(req.params.id);
 
-    const priceCalc: number = req.body.price || product.price;
-    const quantityCalc: number = req.body.quantity || product.quantity;
+		if (!product) {
+			next(new NotFoundError('Product not found'));
+			return;
+		}
+		return res.status(200).json(product);
+	}
 
-    const total_income: number = priceCalc * quantityCalc;
+	async update(
+		req: Request,
+		res: Response,
+		next: NextFunction
+	): Promise<Response | void> {
+		const product = await Product.findByPk(req.params.id);
+		if (!product) {
+			next(new NotFoundError('Product not found'));
+			return;
+		}
 
-    const validProduct: IValidUpdate = await updateProductSchema.validate(
-      req.body,
-      { strict: true }
-    );
+		const priceCalc: number = req.body.price || product.price;
+		const quantityCalc: number = req.body.quantity || product.quantity;
 
-    const alreadyExists = await Product.findOne({
-      where: { name: validProduct.name },
-    });
-    if (alreadyExists) {
-      next(new BadRequestError("Product Name already registered"));
-      return;
-    }
+		const total_income: number = priceCalc * quantityCalc;
 
-    const productEdited = await product.update({
-      ...validProduct,
-      total_income,
-    });
+		const validProduct: IValidUpdate = await updateProductSchema.validate(
+			req.body,
+			{ strict: true }
+		);
 
-    return res.status(200).json({
-      message: "Updated successfully",
-      productEdited,
-    });
-  }
+		const alreadyExists = await Product.findOne({
+			where: { name: validProduct.name },
+		});
+		if (alreadyExists) {
+			next(new BadRequestError('Product Name already registered'));
+			return;
+		}
 
-  async index(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ): Promise<Response | void> {
-    try {
-      const query: IProductQuery = {
-        name: req.query.name ? `%${req.query.name}%` : "",
-        price: req.query.price ? Number(req.query.price) : undefined,
-        sector: req.query.sector ? `${req.query.sector}` : "",
-      };
+		const productEdited = await product.update({
+			...validProduct,
+			total_income,
+		});
 
-      const whereConditions: FindOptions["where"] = {};
+		return res.status(200).json({
+			message: 'Updated successfully',
+			productEdited,
+		});
+	}
 
-      if (query.name) {
-        whereConditions.name = {
-          [Op.like]: query.name,
-        };
-      }
-      if (query.price) {
-        whereConditions.price = {
-          [Op.lte]: query.price,
-        };
-      }
-      if (query.sector) {
-        whereConditions.sector = {
-          [Op.like]: query.sector,
-        };
-      }
+	async index(
+		req: Request,
+		res: Response,
+		next: NextFunction
+	): Promise<Response | void> {
+		try {
+			const query: IProductQuery = {
+				name: req.query.name ? `%${req.query.name}%` : '',
+				price: req.query.price ? Number(req.query.price) : undefined,
+				sector: req.query.sector ? `${req.query.sector}` : '',
+			};
 
-      const products = await Product.findAll({
-        where: whereConditions,
-      });
+			const whereConditions: FindOptions['where'] = {};
 
-      const listOfProducts = products.map((product: IProduct) => {
-        return product;
-      });
+			if (query.name) {
+				whereConditions.name = {
+					[Op.like]: query.name,
+				};
+			}
+			if (query.price) {
+				whereConditions.price = {
+					[Op.lte]: query.price,
+				};
+			}
+			if (query.sector) {
+				whereConditions.sector = {
+					[Op.like]: query.sector,
+				};
+			}
 
-      return res.status(200).json(listOfProducts);
-    } catch (err: any) {
-      next(new BadRequestError(err.message));
-      return;
-    }
-  }
+			const products = await Product.findAll({
+				where: whereConditions,
+			});
 
-  async delete(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ): Promise<Response | void> {
-    try {
-      const product = await Product.findByPk(req.params.id);
+			const listOfProducts = products.map((product: IProduct) => {
+				return product;
+			});
 
-      if (!product) {
-        next(new NotFoundError("Product not found"));
-        return;
-      }
+			return res.status(200).json(listOfProducts);
+		} catch (err: any) {
+			next(new BadRequestError(err.message));
+			return;
+		}
+	}
 
-      await product.destroy();
+	async delete(
+		req: Request,
+		res: Response,
+		next: NextFunction
+	): Promise<Response | void> {
+		try {
+			const product = await Product.findByPk(req.params.id);
 
-      return res.status(204).json("Successfully deleted");
-    } catch (err: any) {
-      next(new BadRequestError(err.message));
-      return;
-    }
-  }
+			if (!product) {
+				next(new NotFoundError('Product not found'));
+				return;
+			}
 
-  async getIncomes(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ): Promise<Response | void> {
-    try {
-      if (!req.query.sector) {
-        next(new NotFoundError("Please provide a sector"));
-        return;
-      }
-      const sector = {
-        sector: req.query.sector ? Number(req.query.sector) : undefined,
-      };
+			await product.destroy();
 
-      const products: ProductInstance[] = await Product.findAll({
-        where: sector,
-      });
+			return res.status(204).json('Successfully deleted');
+		} catch (err: any) {
+			next(new BadRequestError(err.message));
+			return;
+		}
+	}
 
-      if (products.length < 1) {
-        next(new BadRequestError("This sector is not registered"));
-        return;
-      }
+	async getIncomes(
+		req: Request,
+		res: Response,
+		next: NextFunction
+	): Promise<Response | void> {
+		try {
+			if (!req.query.sector) {
+				next(new NotFoundError('Please provide a sector'));
+				return;
+			}
+			const sector = {
+				sector: req.query.sector ? Number(req.query.sector) : undefined,
+			};
 
-      const income = products.reduce(
-        (acumulator: number, prod: ProductInstance) => {
-          return acumulator + Number(prod.total_income);
-        },
-        0
-      );
+			const products: ProductInstance[] = await Product.findAll({
+				where: sector,
+			});
 
-      return res.status(200).json(income);
-    } catch (err: any) {
-      next(new BadRequestError(err.message));
-      return;
-    }
-  }
+			if (products.length < 1) {
+				next(new BadRequestError('This sector is not registered'));
+				return;
+			}
+
+			const income = products.reduce(
+				(acumulator: number, prod: ProductInstance) => {
+					return acumulator + Number(prod.total_income);
+				},
+				0
+			);
+
+			return res.status(200).json(income);
+		} catch (err: any) {
+			next(new BadRequestError(err.message));
+			return;
+		}
+	}
 }
 
 export default new ProductController();
--- backend/src/utils/validators.ts
+++ backend/src/utils/validators.ts
@@ -1,68 +1,67 @@
 import {
-  object,
-  string,
-  number,
-  ObjectSchema,
-  setLocale,
-  ValidationError,
-} from "yup";
-import { IProduct } from "../@types/IProduct";
-import { strict } from "assert";
+	object,
+	string,
+	number,
+	ObjectSchema,
+	setLocale,
+	AnyObjectSchema,
+} from 'yup';
+import { IProduct } from '../@types/IProduct';
 
 export interface IValidProduct {
-  id: string;
-  name: string;
-  quantity: number;
-  price: number;
-  sector: string;
+	id: string;
+	name: string;
+	quantity: number;
+	price: number;
+	sector: string;
 }
 
-export interface IValidUpdate extends Partial<Omit<IValidProduct, "id">> {}
+export interface IValidUpdate extends Partial<Omit<IValidProduct, 'id'>> {}
 
 setLocale({
-  mixed: {
-    default: "Product is invalid",
-  },
-  number: {
-    min: ({ path }) => `${path} Must have more than 1`,
-  },
-  string: {
-    min: "Please insert at least one character",
-    max: ({ path, max }) => `${path} Cannot be more than ${max}`,
-  },
+	mixed: {
+		default: 'Product is invalid',
+	},
+	number: {
+		min: ({ path }) => `${path} Must have more than 1`,
+	},
+	string: {
+		min: 'Please insert at least one character',
+		max: ({ path, max }) => `${path} Cannot be more than ${max}`,
+	},
 });
 
 export const createProductSchema: ObjectSchema<IValidProduct> = object({
-  id: string().required(),
-  name: string().min(1).max(60).required(),
-  quantity: number().min(1).max(5000).required(),
-  price: number().min(1).max(10000).required(),
-  sector: string()
-    .min(0)
-    .max(3)
-    .required()
-    .matches(/^\d+$/, "Please enter only numbers"),
-});
+	id: string().required(),
+	name: string().min(1).max(60).required(),
+	quantity: number().min(1).max(5000).required(),
+	price: number().min(1).max(10000).required(),
+	sector: string()
+		.min(0)
+		.max(3)
+		.required()
+		.matches(/^\d+$/, 'Please enter only numbers'),
+})
 
 export const updateProductSchema: ObjectSchema<IValidUpdate> = object({
-  name: string().min(1).max(60),
-  quantity: number().min(1).max(5000),
-  price: number().min(1).max(10000),
-  sector: string()
-    .min(0)
-    .max(3)
-    .matches(/^\d{1,3}$/, "Please enter only numbers"),
+	name: string().min(1).max(60),
+	quantity: number().min(1).max(5000),
+	price: number().min(1).max(10000),
+	sector: string()
+		.min(0)
+		.max(3)
+		.matches(/^\d{1,3}$/, 'Please enter only numbers'),
 });
 
-export async function isValid<T>(
-  schema: ObjectSchema<T>,
-  data: IProduct
-): Promise<any> {
-	const validationResult = await schema.validate(data, {strict: true, abortEarly: false})
-	.catch(err => {
-		if(err instanceof ValidationError) {
-			return false
-		}
-	})
-	return
+export async function validateData(
+	schema: AnyObjectSchema,
+	productData: IProduct,
+): Promise<IValidProduct | string> {
+	try {
+		const data = await schema.validate(productData);
+		return data;
+	} catch (err: any) {
+    const msg: string = err.message
+		return msg;
+	}
 }
