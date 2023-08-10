8dabacb05cf330ea751ffd30d2d25c9d25a538ef
Victor <linkgamesplays@gmail.com>
2023-08-10 11:46:44 -0300

    fix: remove id field on required validation

--- backend/src/utils/validators.ts
+++ backend/src/utils/validators.ts
@@ -10,7 +10,6 @@ import {
 import { IProduct } from '../@types/IProduct';
 
 export interface IValidProduct {
-	id: string;
 	name: string;
 	quantity: number;
 	price: number;
@@ -39,7 +38,6 @@ setLocale({
 });
 
 export const createProductSchema: ObjectSchema<IValidProduct> = object({
-	id: string().required(),
 	name: string().min(1).max(60).required(),
 	quantity: number().min(1).max(5000).required(),
 	price: number().min(1).max(10000).required(),
