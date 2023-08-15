b8f9043ac82cc54b76ed00742794eec79775a988
victorzix <linkgamesplays@gmail.com>
2023-08-14 23:05:06 -0300

    test: validator createProductSchema

--- tests/utils/validators.spec.ts
+++ tests/utils/validators.spec.ts
@@ -1,24 +1,49 @@
+import { IProduct, ProductInstance } from "../../src/@types/IProduct";
 import { createProductSchema, validateData } from "../../src/utils/validators";
 import { describe, expect, it } from "@jest/globals";
-import { IProduct } from "../../src/@types/IProduct";
 
 describe("Product validation", () => {
-  const data = {
-    id: "123",
-    name: "Test Product",
-    sector: "1",
-    price: 11,
-    quantity: 11,
-  };
-  it("should return a object with data, error and errors", async () => {
-    const validation = await validateData<IProduct>(createProductSchema, data);
+  
 
-    try {
-      expect(validation.data).toEqual(data);
-	  expect(validation.errors).toBe('array')
-      expect(validation.error).toBe('false');
-    } catch (e) {
-      expect(validation.error).toBeTruthy();
-    }
+  it("should validate a product", async () => {
+    const data: IProduct = {
+      name: "Test Product",
+      sector: "1",
+      price: 11,
+      quantity: 11,
+    };
+
+    const validation = await validateData(createProductSchema, data);
+
+    expect(validation.data).toEqual(data);
+    expect(validation.errors).toHaveLength(0);
+    expect(validation.error).toBe(false);
   });
+
+  it("should not allow extra fields from input data", async () => {
+    const data = {
+      name: "Test Product",
+      sector: "1",
+      price: 11,
+      quantity: 11,
+      local: "Rio de Janeiro"
+    };
+
+    const validation = await validateData(createProductSchema, data);
+
+    expect(validation.data).not.toHaveProperty('local');
+  }) 
+
+  it("should not accept wrong data type", async () => {
+    const data = {
+      name: 16,
+      sector: 1,
+      price: "11",
+      quantity: "11"
+    };
+
+    const validation = await validateData(createProductSchema, data as any);
+
+    expect(validation.error).toBe(true)
+  })
 });
